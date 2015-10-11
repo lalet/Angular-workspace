ff931aace0674af152a4b1f925c8222870e5e016
Lalet Scaria <laletscaria@yahoo.co.in>
2015-10-11 12:24:21 +0530

    Angular Commit

---
+++ dives.js
@@ -0,0 +1,19 @@
+var dives = [
+  {
+    site: 'Karaikkal',
+    location: 'Karnataka',
+    depth: 72,
+    time: 54
+  },
+  {
+    site: 'Java Goa',
+    location: 'Mauritius',
+    depth: 54,
+    time: 38
+  },
+  {
+    site: 'Lilly Cave',
+    location: 'Canada',
+    depth: 98,
+    time: 62
+  }];
